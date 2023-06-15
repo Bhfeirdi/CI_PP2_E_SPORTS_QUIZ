035ffb2da1e6847b90a83e36410ae6b94542ddba
Bhfeirdi <ferdia.link@gmail.com>
2023-06-15 03:28:20 +0100

    load question function

--- assest/script.js
+++ assest/script.js
@@ -7,3 +7,19 @@ const resultContainer = document.getElementById('result');
 
 let currentQuestion = 0;
 let score = 0;
+
+function loadQuestion() {
+    const currentQuizData = quizData[currentQuestion];
+    questionContainer.textContent = `${currentQuestion + 1}. ${currentQuizData.question}`;
+    optionContainer.innerHTML = '';
+  
+    currentQuizData.options.forEach((option, index) => {
+      const optionElement = document.createElement('button');
+      optionElement.classList.add('option-btn');
+      optionElement.textContent = option;
+      optionElement.addEventListener('click', checkAnswer);
+      optionContainer.appendChild(optionElement);
+    });
+  }
+
+
