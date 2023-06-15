36094438b491df5e1b4506addb300f542606f1d9
Bhfeirdi <ferdia.link@gmail.com>
2023-06-15 03:25:23 +0100

    create const list

---
+++ assest/script.js
@@ -0,0 +1,6 @@
+const startButton = document.getElementById('start-btn');
+const startContainer = document.getElementById('start-container');
+const quizContainer = document.getElementById('quiz-container');
+const questionContainer = document.getElementById('question');
+const optionContainer = document.getElementById('options');
+const resultContainer = document.getElementById('result');
